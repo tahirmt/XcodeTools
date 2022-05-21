ad5ad2048bd4c5815d3f3faedea91d449f71253b
Mahmood Tahir <mahmood.tahir@thescore.com>
2022-05-21 01:13:46 -0400

    Initial Commit

---
+++ XcodeTools/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XcodeTools
+//
+//  Created by Mahmood Tahir on 2022-05-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XcodeTools/XcodeToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeToolsApp.swift
+//  XcodeTools
+//
+//  Created by Mahmood Tahir on 2022-05-21.
+//
+
+import SwiftUI
+
+@main
+struct XcodeToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
